04695f0095b9185119df280e8713023a4b6b374e
davisv7 <davisv@berea.edu>
2018-12-15 16:22:11 -0500

    boarding in 15 minutes

--- main.py
+++ main.py
@@ -3,10 +3,46 @@ from bs4 import BeautifulSoup as bs
 import time
 from os import getcwd, makedirs
 from os.path import join, exists
+from re import sub
 
 
-def fix_title(title):
-    return "".join(x for x in title.replace(' ', '_') if x.isalnum() or x == '_')
+class FEMloader:
+
+    def __init__(self):
+        # self.website = input('url?')
+        self.website = 'https://freeallmusic.top/album/3283-6611-3447'
+
+        first_soup = make_soup(self.website)
+        song_box = first_soup.find('div', attrs={'class': 'col-lg-9 b-r no-border-md'})
+        titles_links = song_box.find_all('div', attrs={'class': 'item-title text-ellipsis'})
+        self.artist = self.fix_string(first_soup.find('a', attrs={'class': 'item-author _600'}).next.text)
+        print(self.artist)
+        self.album = self.fix_string(sub('[\ \n]{2,}', '', first_soup.find('h4', attrs={'class': 'inline'}).text))
+        print(self.album)
+        cwd = getcwd()
+        self.location = join(cwd, 'albums', self.artist, self.album)
+
+        for item in titles_links:
+            anchor = item.find('a')
+            self.title, pre_link = anchor.text, anchor['href']
+            print(self.title)
+            pre_soup = make_soup(pre_link)
+            self.referer = pre_soup.find('a', attrs={'class': 'text-danger'})['href']
+            page_soup = make_soup(self.referer)
+            self.download_link = page_soup.find('div', attrs={'class': 'card-body px-0'}).a['href']
+            self.save_song()
+
+    def fix_string(self, string):
+        return "".join(x for x in string.replace(' ', '_') if x.isalnum() or x == '_')
+
+    def save_song(self):
+        song = requests.get(self.download_link, headers={'referer': self.referer})
+        if not exists(self.location):
+            makedirs(self.location)
+        with open(f'{join(self.location,self.fix_string(self.title))}.mp3', 'wb') as f:
+            f.write(song.content)
+        print(f'downloaded {self.title}, sleeping 3 seconds')
+        time.sleep(3)
 
 
 def make_soup(link):
@@ -16,40 +52,8 @@ def make_soup(link):
     return soup
 
 
-def save_song(title, artist, link, referer):
-    cwd = getcwd()
-    path = join(cwd, artist)
-    song = requests.get(link, headers={'referer': referer})
-    if not exists(path):
-        makedirs(path)
-    with open(f'{join(path,title)}.mp3', 'wb') as f:
-        f.write(song.content)
-    print(f'downloaded {title}, sleeping 3 seconds')
-    time.sleep(3)
-
-
 def main():
-    # website = input('url?')
-    website = 'https://freeallmusic.top/album/3283-6611-3447'
-
-    a_soup = make_soup(website)
-    box = a_soup.find('div', attrs={'class': 'col-lg-9 b-r no-border-md'})
-    songs_titles_links = box.find_all('div', attrs={'class': 'item-title text-ellipsis'})
-    artist = a_soup.find('a', attrs={'class': 'item-author _600'}).next.text
-    print(artist)
-    for item in songs_titles_links:
-        anchor = item.find('a')
-        song_title = anchor.text
-        print(song_title)
-        pre_song_link = anchor['href']
-        pre_soup = make_soup(pre_song_link)
-        song_link = pre_soup.find('a', attrs={'class': 'text-danger'})['href']
-        page_soup = make_soup(song_link)
-
-        download_link = \
-            page_soup.find('a', attrs={'class': 'btn btn-outline-success text-uppercase mt-3 font-weight-bold db'})[
-                'href']
-        save_song(song_title, artist, download_link, song_link)
+    FEMloader()
 
 
 if __name__ == '__main__':
