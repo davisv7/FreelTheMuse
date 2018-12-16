9d99723677582ce2f681f7207ab2f289101f27c8
davisv7 <davisv@berea.edu>
2018-12-16 16:24:53 -0500

    and now its a web app

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import time
 from os import getcwd, makedirs
 from os.path import join, exists
 from re import sub
+import shutil
 
 
 class FEMloader:
 
-    def __init__(self):
-        # self.website = input('url?')
-        self.website = 'https://freeallmusic.top/album/3283-6611-3447'
+    def __init__(self, url):
 
+        self.website = url
         first_soup = make_soup(self.website)
         song_box = first_soup.find('div', attrs={'class': 'col-lg-9 b-r no-border-md'})
         titles_links = song_box.find_all('div', attrs={'class': 'item-title text-ellipsis'})
@@ -20,7 +20,14 @@ class FEMloader:
         self.album = self.fix_string(sub('[\ \n]{2,}', '', first_soup.find('h4', attrs={'class': 'inline'}).text))
         print(self.album)
         cwd = getcwd()
-        self.location = join(cwd, 'albums', self.artist, self.album)
+        self.dl_location = join(cwd, 'albums', self.artist, self.album)
+        self.zip_location = join(cwd, 'zips')
+        self.zip_name = join('zips', self.album + '.zip')
+
+        if not exists(self.dl_location):
+            makedirs(self.dl_location)
+        if not exists(self.zip_location):
+            makedirs(self.zip_location)
 
         for item in titles_links:
             anchor = item.find('a')
@@ -31,18 +38,20 @@ class FEMloader:
             page_soup = make_soup(self.referer)
             self.download_link = page_soup.find('div', attrs={'class': 'card-body px-0'}).a['href']
             self.save_song()
+        self.make_zip()
 
     def fix_string(self, string):
         return "".join(x for x in string.replace(' ', '_') if x.isalnum() or x == '_')
 
     def save_song(self):
         song = requests.get(self.download_link, headers={'referer': self.referer})
-        if not exists(self.location):
-            makedirs(self.location)
-        with open(f'{join(self.location,self.fix_string(self.title))}.mp3', 'wb') as f:
+        with open(f'{join(self.dl_location,self.fix_string(self.title))}.mp3', 'wb') as f:
             f.write(song.content)
         print(f'downloaded {self.title}, sleeping 3 seconds')
         time.sleep(3)
+
+    def make_zip(self):
+        shutil.make_archive(join(self.zip_location, self.album), 'zip', self.dl_location)
 
 
 def make_soup(link):
@@ -50,11 +59,3 @@ def make_soup(link):
         soup = bs(webobj.content, "html.parser")
     time.sleep(1)
     return soup
-
-
-def main():
-    FEMloader()
-
-
-if __name__ == '__main__':
-    main()
